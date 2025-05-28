9d60375dff9dc1b1eb1e9263bb0ad02d5f88c7eb
Ritwik Singh <97968834+datasciritwik@users.noreply.github.com>
2025-05-28 21:30:14 +0530

    Update routing to only keep interview page

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import App from './App.tsx'
-import JobDetailPage from './components/JobDetailPage.tsx'
-import UserInstructions from './components/UserInstructions.tsx'
-import './index.css'
 import VideoChatWithExecution from './components/ImprovedVideoChat.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/job/:jobId" element={<JobDetailPage />} />
-        <Route path="/instructions" element={<UserInstructions />} />
-        <Route path="/interview" element={<VideoChatWithExecution />} />
+        <Route path="/" element={<VideoChatWithExecution />} />
       </Routes>
     </Router>
   </React.StrictMode>,
